1b9f987e56d911a45c1e88a150889adeb16ebaf9
vishnu <vishnuvrdhn48@gmail.com>
2019-09-29 06:25:43 -0500

    intial commit

---
+++ src/Eample.java
@@ -0,0 +1,5 @@
+public class Eample {
+    public static void main(String[] args) {
+        System.out.println("hey how are you");
+    }
+}
---
+++ src/main/java/com/docker/demo/pojo.java
@@ -0,0 +1,13 @@
+package com.docker.demo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class pojo {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
